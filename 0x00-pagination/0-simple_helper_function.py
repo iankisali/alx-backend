03f744ce8f01bd40f96084426a9f918d02a4e0e0
Ian Kisali <iankisali@gmail.com>
2023-05-13 18:02:07 +0300

    function returning tuple with starting & ending index

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""function index_range that takes two integer arguments page and page_size"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returns the tuple"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
