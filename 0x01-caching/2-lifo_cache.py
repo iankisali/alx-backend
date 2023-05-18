7618e3020db97bca7590716596b0b1966d317515
Ian Kisali <iankisali@gmail.com>
2023-05-18 16:54:42 +0300

    LIFO Caching method

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""LIFO Caching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """class LIFOCache inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        """init method"""
+        super().__init__()
+        self.key_indexes = []
+
+    def put(self, key, item):
+        """Put method"""
+        if key and item:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                if key in self.cache_data:
+                    del self.cache_data[key]
+                    self.key_indexes.remove(key)
+                else:
+                    del self.cache_data[self.key_indexes[self.MAX_ITEMS - 1]]
+                    discard = self.key_indexes.pop(self.MAX_ITEMS - 1)
+                    print("DISCARD:", discard)
+            self.cache_data[key] = item
+            self.key_indexes.append(key)
+
+    def get(self, key):
+        """get method"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
