013d479e173231b9be1972893c4de784410ce5db
Ian Kisali <iankisali@gmail.com>
2023-05-19 17:24:47 +0300

    LFU Caching method

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    """class LFUCache that inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        """Init method"""
+        super().__init__()
+        self.temp_list = {}
+
+    def put(self, key, item):
+        """Put method"""
+        if not (key is None or item is None):
+            self.cache_data[key] = item
+            if len(self.cache_data.keys()) > self.MAX_ITEMS:
+                pop = min(self.temp_list, key=self.temp_list.get)
+                self.temp_list.pop(pop)
+                self.cache_data.pop(pop)
+                print(f"DISCARD: {pop}")
+            if not (key in self.temp_list):
+                self.temp_list[key] = 0
+            else:
+                self.temp_list[key] += 1
+
+    def get(self, key):
+        """Get method"""
+        if (key is None) or not (key in self.cache_data):
+            return None
+        self.temp_list[key] += 1
+        return self.cache_data.get(key)
