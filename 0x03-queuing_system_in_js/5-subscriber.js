950919e3889a0c05a084e26b59c21dd3a20eb14a
Ian Kisali <iankisali@gmail.com>
2023-06-21 15:37:07 +0300

    node Redis client publisher and subscriber

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,21 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('error', (err) => { 
+    console.log(`Redis client not connected to the server: ${err}`) 
+});
+client.on('connect', () => { 
+    console.log('Redis client connected to the server') 
+});
+
+client.subscribe('holberton school channel');
+client.on('message', (channel, message) => {
+    if (channel === 'holberton school channel') {
+        console.log(message);
+    }
+    if (message === 'KILL_SERVER') {
+        client.unsubscribe('holberton school channel');
+        client.quit();
+    }
+});
